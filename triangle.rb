0499911705966706c99996704f8824f33d7a8c7f
Richard Davis <rdavis.bacs@gmail.com>
2013-09-08 20:41:09 -0400

    sandwich code

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,21 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a <= 0 or b <= 0 or c <= 0
+    raise TriangleError
+  end
+
+  if a + b <= c or b + c <= a or a + c <= b
+    raise TriangleError
+  end
+
+  if a == b and b == c
+    return :equilateral
+  elsif a == b or b == c or a == c
+    return :isosceles
+  else
+    return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
